9c932fef781ae598c94d1a21086ab2869ab051e1
Samuel Casey <76224164+Jeve-Stobs@users.noreply.github.com>
2021-03-15 09:49:43 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,72 @@
+/** @format */
+
+const OFFLINE_VERSION = 1;
+const cacheName = "offline";
+const OFFLINE_URL = "./offline.html";
+
+self.addEventListener("fetch", function (event) {
+	event.respondWith(
+		caches.open("offline").then(function (cache) {
+			return cache.match(event.request).then(function (response) {
+				return (
+					response ||
+					fetch(event.request).then(function (response) {
+						cache.put(event.request, response.clone());
+						return response;
+					})
+				);
+			});
+		})
+	);
+});
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		(async () => {
+			caches.open(cacheName).then(function (cache) {
+				return cache.addAll([
+					"./index.html",
+					"./script.js",
+					"./style.css",
+          "https://apprecs.org/gp/images/app-icons/300/00/com.paresh.connectingdots.jpg",
+          "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+				]);
+			});
+		})()
+	);
+});
+
+self.addEventListener("activate", (event) => {
+	event.waitUntil(
+		(async () => {
+			if ("navigationPreload" in self.registration) {
+				await self.registration.navigationPreload.enable();
+			}
+		})()
+	);
+
+	self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+	if (event.request.mode === "navigate") {
+		event.respondWith(
+			(async () => {
+				try {
+					const preloadResponse = await event.preloadResponse;
+					if (preloadResponse) {
+						return preloadResponse;
+					}
+
+					const networkResponse = await fetch(event.request);
+					return networkResponse;
+				} catch (error) {
+					console.log("Fetch failed; returning offline page instead.", error);
+
+					const cache = await caches.open(cacheName);
+					const cachedResponse = await cache.match(OFFLINE_URL);
+					return cachedResponse;
+				}
+			})()
+		);
+	}
+});
